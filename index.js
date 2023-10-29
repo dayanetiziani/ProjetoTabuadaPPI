c97334ab4b28b074d69c9094f02bdcb02dc5447a
dayanetiziani@hotmail.com <dayanetiziani@hotmail.com>
2023-10-29 12:29:54 -0300

    Adicionado recurso de Tabuada

--- index.js
+++ index.js
@@ -2,12 +2,72 @@ import express from 'express';
 
 const app = express();
 
-const host = '0.0.0.0'; //ip genérico que representa todas as interfaces de rede (placas de rede)
-const porta = 3000; //porta identifica uma aplicação, dentro unúmeras, que podem estar sendo
+const host = '0.0.0.0'; //ip genérico que representa todas as interfaces de rede (placas de rede) locais do
+const porta = 3000; //porta identifica uma aplicação, dentro unúmeras, que podem estar sendo executadas no 
 
+
+//paginaInicial está sendo passada como parametro para esse método
+app.get('/',paginaInicial);
+app.get('/tabuada', processarTabuada);
+
+// () => {} // é uma função anônima (arrow function ou função de seta)
+app.listen(porta, host, () => {
+    //main - escrever o código aqui dentro
+    //string literals permite concatenar variaveis com strings de uma maneira
+    //string literals = ``
+    console.log(`Servidor executando em http://${host}:${porta}.`);
+
+} );
+
+function processarTabuada(requisicao, resposta){
+    try{//aqui é se for informado um numero
+        let numero = Number(requisicao.query.numero);
+        let conteudoResposta = `<!DOCTYPE html>
+            <html lang="pt-br">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Tabuada|${numero}</title>
+            </head>
+            <body>
+                <h1> Tabuada ${numero} </h1>
+                <hr>
+                <ul>
+    `;
+    for(let i=1; i<=10; i++) {
+        let linha = `<li>${numero} * ${i} = ${numero*i} </li>`;
+        conteudoResposta += linha;
+    } 
+    conteudoResposta+= `
+                </ul>
+            </body>
+        </html>
+    `;
+    resposta.end(conteudoResposta); 
+        
+    }catch (erro) {
+        resposta.end(`
+        <!DOCTYPE html>
+            <html lang="pt-br">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Tabuada|Erro</title>
+                </head>
+                <body>
+                    <h1>Erro ao processar Tabuada</h1>
+                    <hr>
+                    <h2>Não foi possivel processar a sua requisição,
+                    <h3>${erro.message} 
+                </body>  
+            </html>       
+        `);
+        resposta.end()};
+            
+}
 //requisição e a resposta são parametros que o express automaticamente passará para sua função
 function paginaInicial(requisicao, resposta){
-    requisicao.
+    
     resposta.send( `<!DOCTYPE html>
     <html lang="pt-br">
     <head>
@@ -45,14 +105,3 @@ function paginaInicial(requisicao, resposta){
     resposta.end();
 
 }
-//paginaInicial está sendo passada como parametro para esse método
-app.get('/',paginaInicial);
-
-// () => {} // é uma função anônima (arrow function ou função de seta)
-app.listen(porta, host, () => {
-    //main - escrever o código aqui dentro
-    //string literals permite concatenar variaveis com strings de uma maneira
-    //string literals = ``
-    console.log(`Servidor executando em http://${host}:${porta}.`);
-
-} );
